e97478eb4d33d984534435efca1812a9a76d338f
HE77XTL <he77505357@163.com>
2018-05-23 21:46:56 +0800

    hot-song a href

--- src/js/index.js
+++ src/js/index.js
@@ -41,13 +41,13 @@ $(()=>{
 		if(index == 1){
 			$.get('./data/page2.json').then((result)=>{
 				// 获取热歌榜信息
-				// console.log(result)
+				console.log(result)
 				let items = result;
 				// 其实可以和上面的代码合并，但是html和css尝试了两种方法，作为练习就这样了
 				items.forEach((value,index)=>{
 					let $li = $(`
 						<li>
-							<a href="#">
+							<a href="playing.html?id=${value.id}">
 								<span>${index + 1}</span>
 								<div class="detail">
 									<h2>${value.name}</h2>
